23e054e34bd037f91bbd1cf3631e8822ab68c6b8
asairamyasri <sri.ramyasri09@gmail.com>
2026-07-14 07:24:11 +0530

    Allow CORS for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ Base.metadata.create_all(bind=engine)
 # Allow React frontend to access this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
